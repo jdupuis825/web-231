32d0916900bda9211032bf32d04e7856b146b0ac
Jocelyn Dupuis <jdupuis@my365.bellevue.edu>
2023-03-31 10:21:05 -0400

    Add files via upload

---
+++ week-3/dupuis-favorites.js
@@ -0,0 +1,29 @@
+/*
+============================================
+; Title: dupuis-favorites.js
+; Author: Jocelyn Dupuis
+; Date: 03/31/2023
+; Description: JavaScript file for My Favorite Apps
+============================================
+*/
+
+// Onlcick function to display window alert message for my favorite sports team
+function favTeam() {
+  alert('My favorite team is the New York Rangers')
+}
+
+// Onlcick function to display window alert message for my favorite book
+function favBook() {
+  alert('My favorite book is Some Awful Cunning')
+}
+
+// Onlcick function to display window alert message for my favorite author
+function favAuthor() {
+  alert('My favorite author is Joe Ricker')
+}
+
+// Onlcick function to display window alert message for my favorite programming language
+function favProgrammingLanguage() {
+  alert('My favorite programming language is JavaScript')
+}
+
